413900e24048e9b32418a8fafde073ffe6f5a7fc
abhithemauryas <abhithemauryas5@gmail.com>
2024-03-19 03:44:24 +0530

    final commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
 function App() {
+  const demo=()=>{
+    const a=10;
+    const b=10;
+  }
   return (
     <div className="App">
       <AllRoutes />
