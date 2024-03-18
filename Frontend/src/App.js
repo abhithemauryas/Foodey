20a4b7b9064ba9329def1c6097bdb01b5fb28236
abhithemauryas <abhithemauryas5@gmail.com>
2024-03-19 03:46:04 +0530

    final commit for deployment

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,10 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
 function App() {
-  const demo=()=>{
-    const a=10;
-    const b=10;
-  }
+ 
   return (
     <div className="App">
       <AllRoutes />
